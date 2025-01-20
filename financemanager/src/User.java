c9e94f39cbc30264407d383f4db13344763b788f
DJahich <r.kassanov@djahich.ru>
2025-01-20 18:32:33 +0300

    added the project

---
+++ financemanager/src/User.java
@@ -0,0 +1,25 @@
+package financemanager;
+
+public class User {
+    private String username;
+    private String password;
+    private Wallet wallet;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.wallet = new Wallet();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+}
